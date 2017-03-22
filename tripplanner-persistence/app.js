3fc64b21440bcb8f8aafbbe9ca3b5f818d661516
Yejee Arin Choi <yej.arin.choi@gmail.com>
2017-03-22 16:30:26 -0400

    able to get json data on api pages

--- tripplanner-persistence/app.js
+++ tripplanner-persistence/app.js
@@ -5,6 +5,7 @@ var morgan = require('morgan');
 var nunjucks = require('nunjucks');
 var db = require('./models');
 var router = require('./routes');
+var apiRouter = require('./routes/api')
 
 
 var app = express();
@@ -22,7 +23,8 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 app.use(morgan('dev'))
-app.use(router);
+app.use('/', router)
+app.use('/api', apiRouter);
 
 app.use(function(err, req, res, next) {
 	console.error(err)
---
+++ tripplanner-persistence/routes/api.js
@@ -0,0 +1,31 @@
+var Promise = require('bluebird');
+var router = require('express').Router();
+
+var db = require('../models');
+var Hotel = db.model('hotel');
+var Restaurant = db.model('restaurant');
+var Activity = db.model('activity');
+var Place = db.model('place');
+
+router.get('/hotels', function(req, res, next) {
+  Hotel.findAll()
+  .then((hotels) => res.json(hotels))
+  .catch(next)
+})
+
+
+router.get('/restaurants', function(req, res, next) {
+  Restaurant.findAll()
+  .then((restaurants) => res.json(restaurants))
+  .catch(next)
+})
+
+
+router.get('/activities', function(req, res, next) {
+  Activity.findAll()
+  .then((activities) => res.json(activities))
+  .catch(next)
+})
+
+
+module.exports = router;
